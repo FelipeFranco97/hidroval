a13a0bd1acf24676cf3d42c28611554f4bcecd4c
Felipe Franco <francozuluaga97@gmail.com>
2025-05-30 20:09:09 -0500

    Actualiza el título y la descripción del sitio para reflejar la identidad y experiencia de Hidroval S.A.S.

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Hidroval";
+export const SITE_TITLE = "Hidroval S.A.S - Ingeniería al Serivcio del Agua";
 export const SITE_DESCRIPTION =
-  "Su mejor aliado en infraestructura hidráulica.";
+  "Su mejor aliado en infraestructura hidráulica con más de 20 años de experiencia en acueducto y alcantarillado.";
